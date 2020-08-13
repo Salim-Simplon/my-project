47a08e939aab878f21705e2caf315d784b6cde5a
Salim-Simplon <amri.salim.simplon@gmail.com>
2020-08-13 09:25:09 +0100

    adding style for the product component

--- src/components/Produits.js
+++ src/components/Produits.js
@@ -1,32 +1,35 @@
 import React, { Component } from "react";
 import formatPrix from "./Prix";
+import Fade from "react-reveal/Fade";
 
 export default class Produits extends Component {
   render() {
     return (
       <div>
-        <ul className="products">
-          {this.props.products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
-                <img src={product.photo} alt={product.titre}></img>
-                <a href={"#" + product._id}>
-                  {" "}
-                  <p className="title">{product.titre}</p>
-                </a>
-                <p className="prix-produit">
-                  <div>{formatPrix(product.prix)}</div>
-                  <button
-                    onClick={() => this.props.addPanier(product)}
-                    className="button primary"
-                  >
-                    Ajouter au panier
-                  </button>
-                </p>
-              </div>
-            </li>
-          ))}
-        </ul>
+        <Fade bottom cascade={true}>
+          <ul className="products">
+            {this.props.products.map((product) => (
+              <li key={product._id}>
+                <div className="product">
+                  <img src={product.photo} alt={product.titre}></img>
+                  <a href={"#" + product._id}>
+                    {" "}
+                    <p className="title">{product.titre}</p>
+                  </a>
+                  <p className="prix-produit">
+                    <div>{formatPrix(product.prix)}</div>
+                    <button
+                      onClick={() => this.props.addPanier(product)}
+                      className="button primary"
+                    >
+                      Ajouter au panier
+                    </button>
+                  </p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Fade>
       </div>
     );
   }
