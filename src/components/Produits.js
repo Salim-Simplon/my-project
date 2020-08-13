911c704dd93557bcdeeee486769553b8be7ec7fc
Salim-Simplon <amri.salim.simplon@gmail.com>
2020-08-13 12:24:23 +0100

    Adding modal product

--- src/components/Produits.js
+++ src/components/Produits.js
@@ -1,9 +1,21 @@
 import React, { Component } from "react";
 import formatPrix from "./Prix";
 import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
+import Modal from "react-modal";
 
 export default class Produits extends Component {
+  state = {
+    product: null,
+  };
+  openModal = (product) => {
+    this.setState({ product });
+  };
+  closeModal = () => {
+    this.setState({ product: null });
+  };
   render() {
+    const { product } = this.state;
     return (
       <div>
         <Fade bottom cascade={true}>
@@ -11,9 +23,11 @@ export default class Produits extends Component {
             {this.props.products.map((product) => (
               <li key={product._id}>
                 <div className="product">
-                  <img src={product.photo} alt={product.titre}></img>
-                  <a href={"#" + product._id}>
-                    {" "}
+                  <a
+                    href={"#" + product._id}
+                    onClick={() => this.openModal(product)}
+                  >
+                    <img src={product.photo} alt={product.titre}></img>{" "}
                     <p className="title">{product.titre}</p>
                   </a>
                   <p className="prix-produit">
@@ -30,6 +44,31 @@ export default class Produits extends Component {
             ))}
           </ul>
         </Fade>
+        {product && (
+          <Modal isOpen={true} onRequestClose={this.closeModal}>
+            <Zoom>
+              <button className="close-modal" onClick={this.closeModal}>
+                x
+              </button>
+              <div className="produit-description">
+                <img src={product.photo} alt={product.titre}></img>
+                <div className="produit-description-detail">
+                  <p className="titre-modal">{product.titre}
+                  </p>
+                  <p>
+                    <strong>Description : </strong> {product.description}
+                  </p>
+                  <p>
+                    <strong>Etat : </strong> {product.etat}
+                  </p>
+                  <p>
+                    <strong>Prix : </strong> {formatPrix(product.prix)}
+                  </p>
+                </div>
+              </div>
+            </Zoom>
+          </Modal>
+        )}
       </div>
     );
   }
